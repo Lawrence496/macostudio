42516d43c1799049082993da643520a8d833d2f3
Lawrence Anyalechi <58028325+Lawrence496@users.noreply.github.com>
2024-06-25 02:33:03 +0100

    Update settings.py

--- macostudio/settings.py
+++ macostudio/settings.py
@@ -153,7 +153,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'macostudio/static'),
-
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
